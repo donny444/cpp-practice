b66cb2d8bf3a58bba94f6b5cbb7dd19908e662d2
donny_444 <104888835+donny444@users.noreply.github.com>
2023-11-23 23:35:38 +0700

    Add files via upload

---
+++ vscode.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World" << std::endl;
+    return 0;
+}
+
+/* This is C++ Walkthrough by VS Code. Including setting up enviroment, installing compiler */
