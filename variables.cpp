b3eb48d8648e5d8238c065f8d53680dc67e9221e
donny_444 <104888835+donny444@users.noreply.github.com>
2023-11-25 23:31:45 +0700

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myInt = 444;
+    double myDouble = 19.99;
+    char myChar = 'D';
+    string myString = "Donny";
+    bool myBool = false;
+    cout << myInt;
+    cout << myDouble;
+    cout << myChar;
+    cout << myString;
+    cout << myBool;
+    return 0;
+}
