e90a6e744d874e21201b29494ff1f4234e1cead5
donny_444 <104888835+donny444@users.noreply.github.com>
2023-11-28 22:12:32 +0700

    Add files via upload

---
+++ constant.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int minutesPerHour = 60;
+    const float PI = 3.14;
+    const float e = 2.17;
+    const int bitsPerByte = 8;
+    const string myNickName = "Donny";
+
+    cout << myNickName;
+    return 0;
+}
---
+++ numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myInt = 1000;
+    float myFloat = 5.75;
+    double myDouble = 19.99;
+
+    //Scientific Numbers
+    float f1 = 35e3;
+    double d1 = 12e4;
+    cout << f1;
+    cout << d1;
+    
+    return 0;
+}
