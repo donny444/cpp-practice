66f937f37cffe51fd3fc71e11f745a010c1c44be
donny_444 <104888835+donny444@users.noreply.github.com>
2023-12-08 22:45:13 +0700

    Add files via upload

---
+++ asking.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string food = "Boba Tea";
+	string* foodAddress = &food;
+	int ans;
+	cout << "What you'd like to know\n";
+	cout << "[1] The Food\n";
+	cout << "[2] Food Address\n";
+	cin >> ans;
+	
+	switch(ans) {
+		case 1: cout << food; break;
+		case 2: cout << foodAddress; break;
+		default: cout << "Input only 'a' or 'b' (case-sensitive)";
+	}
+	
+	return 0;
+}
